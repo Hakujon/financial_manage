f5e4d410d55ade9dc94718ea3a03ed0e263f8f87
Hakujona <stlesha1992@gmail.com>
2025-09-07 16:42:25 +0300

    add categories and plan schemas

--- app/finances/schemas.py
+++ app/finances/schemas.py
@@ -3,14 +3,33 @@ from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field
 
 
+class BaseCategory(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str = Field(
+        ..., min_length=3, max_length=15,
+        description="Название категории"
+    )
+
+
 class BaseExpense(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     amount: float = Field(
         ..., ge=1, description="Сумма"
     )
-    category: str = Field(
-        ..., min_length=3, max_length=35, description="Категория"
-        )
+    category: BaseCategory
+
+
+class BasePlan(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    category: BaseCategory
+
+
+class CreateCategory(BaseCategory):
+    pass
+
+
+class CreateExpense(BaseExpense):
     description: Optional[str] = Field(
         None,
         min_length=5,
@@ -19,14 +38,52 @@ class BaseExpense(BaseModel):
     )
 
 
-class CreateExpense(BaseExpense):
-    pass
+class CreatePlan(BasePlan):
+    start_date: Optional[datetime] = Field(
+        None,
+        description="Начало промежутка планирования"
+    )
+    planned_amount: Optional[float] = Field(
+        None,
+        description="Планируемая сумма"
+    )
 
 
 class ResponseExpense(BaseExpense):
     id: int
-    created_at: Optional[datetime] = Field(
-        None, description="Дата создания"
+    description: Optional[str] = Field(
+        None,
+        min_length=5,
+        max_length=80,
+        description="Краткое описание"
+    )
+    created_at: datetime = Field(
+        ..., description="Дата создания"
+    )
+
+
+class ResponseCategory(BaseCategory):
+    id: int
+
+
+class ResponsePlan(BasePlan):
+    id: int
+    start_date: datetime = Field(
+        ..., description="Начало промежутка планирования"
+    )
+    end_date: datetime = Field(
+            ..., description="Конец промежутка планирования"
+    )
+    planned_amount: float = Field(
+        ..., description="Планируемая сумма"
+    )
+    created_at: datetime = Field(
+        ...,
+        description="Дата создания плана"
+    )
+    updated_at: datetime = Field(
+        ...,
+        description="Дата последнего изменения"
     )
 
 
