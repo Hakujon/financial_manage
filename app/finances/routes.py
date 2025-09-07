d133b2aa3605bf530fee64f834fa935fc6a91e60
Hakujona <stlesha1992@gmail.com>
2025-09-07 16:45:18 +0300

    rework endpoints to new logic

--- app/finances/routes.py
+++ app/finances/routes.py
@@ -1,77 +1,268 @@
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter, Depends,
+    HTTPException, status
+    )
 from typing import Annotated
-from app.finances.schemas import ResponseExpense, CreateExpense, FilterExpense
-from app.finances.dao import ExpenseDAO
+
+from app.finances.service import (
+    CategoryService,
+    PlanService,
+    ExpenseService
+)
+from app.finances.schemas import (
+    ResponseExpense, CreateExpense, FilterExpense,
+    BaseCategory, CreatePlan, ResponseCategory, ResponsePlan
+)
 from app.database import DB_SESSION
 from app.finances.caching import get_cache, CacheClient
-from app.finances.service import find_expenses_by_filter_with_cache
+from app.exceptions.exceptions import (
+    DatabaseException, CacheExcpeption,
+    ServiceException, NotFoundException
+)
 
 
-router = APIRouter(prefix="/finance", tags=["Work with Expenses"])
+router = APIRouter(prefix="/finance", tags=[
+    "Work with Expenses, Categories and Plans"])
 
 
-# @router.get("/")
-# async def get_expenses_by_filter(
-#     request_body: Annotated[FilterExpense, Depends()],
-#     db_session: DB_SESSION,
-# ) -> list[ResponseExpense] | dict:
-#     expenses = await ExpenseDAO.find_by_filter(
-#         db_session, request_body)
-#     if not expenses:
-#         return {"message":
-#                 "Expenses not found"}
-#     return [ResponseExpense.model_validate(expense) for expense in expenses]
-
-@router.get("/")
-async def get_expenses_by_filter(
+@router.get("/expenses")
+async def get_expenses_by_filter_with_cache(
     request_body: Annotated[FilterExpense, Depends()],
-    db_session: DB_SESSION
-):
-    cache = await get_cache()
-    expenses = await find_expenses_by_filter_with_cache(db_session,
-                                                        request_body,
-                                                        cache)
-    if not expenses:
-        return {"message":
-                "Expenses not found"}
-    return expenses
-
-@router.get("/all")
-async def get_all_expenses(
-    db_session: DB_SESSION
+    db_session: DB_SESSION,
+    cache: CacheClient = Depends(get_cache)
 ) -> list[ResponseExpense] | dict:
-    expenses = await ExpenseDAO.find_all(db_session)
-    if not expenses:
-        return {"message": "NOT found"}
-    return [ResponseExpense.model_validate(expense) for expense in expenses]
+    try:
+        expenses = await ExpenseService.get_expenses_with_cache(
+            db_session=db_session,
+            expense_filter=request_body,
+            cache=cache
+        )
+        if not expenses:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="expenses wasn't found"
+            )
+        return [ResponseExpense.model_validate(expense)
+                for expense in expenses]
+    except (DatabaseException, CacheExcpeption) as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
 
 
-@router.post("/")
+@router.post("/expenses")
 async def create_expense(
     expense: CreateExpense,
     db_session: DB_SESSION
-):
-    expense_to_db = await ExpenseDAO.add(db_session, **expense.model_dump())
-    return {"message": "Expense was added",
-            "expense": ResponseExpense.model_validate(expense_to_db)}
-
-
-@router.delete("/{data_id}")
-async def delete_expense(
-    data_id: int,
-    db_session: DB_SESSION
-):
+) -> ResponseExpense:
     try:
-        await ExpenseDAO.delete(db_session, data_id=data_id)
-    except Exception as e:
-        await db_session.rollback()
-        return e
-    return {"message":
-            "Expense was deleted"}
+        new_expense = await ExpenseService.create_expense(
+            db_session=db_session,
+            expense=expense
+        )
+
+        return ResponseExpense.model_validate(new_expense)
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.delete("/expenses/{expense_id}")
+async def delete_expense(
+    expense_id: int,
+    db_session: DB_SESSION
+) -> dict[str, str]:
+    try:
+        result_of_delete = await ExpenseService.delete_expense(
+            db_session=db_session,
+            expense_id=expense_id
+        )
+        if result_of_delete:
+            return {"message":
+                    "expense was deleted"}
+        else:
+            raise ServiceException(
+                "Expense wasn't deleted"
+            )
+    except ServiceException as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.post("/plans")
+async def create_new_plan(
+    plan: CreatePlan,
+    db_session: DB_SESSION
+) -> ResponsePlan:
+    try:
+        new_plan = await PlanService.create_plan(
+            db_session=db_session,
+            plan=plan
+        )
+        return ResponsePlan.model_validate(new_plan)
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.get("/plans")
+async def find_plan(
+    category: BaseCategory,
+    db_session: DB_SESSION
+) -> ResponsePlan:
+    try:
+        plan_from_db = await PlanService.get_plan(
+            db_session=db_session,
+            category=category
+        )
+        if not plan_from_db:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Plan not found"
+            )
+        return ResponsePlan.model_validate(plan_from_db)
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.put("/plans/{plan_id}")
+async def update_plan(
+    plan_id: int,
+    db_session: DB_SESSION,
+    plan: CreatePlan
+) -> ResponsePlan:
+    try:
+        updated_plan = await PlanService.update_plan(
+            db_session=db_session,
+            plan_id=plan_id,
+            plan=plan
+        )
+        if updated_plan is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Plan not found"
+            )
+        return ResponsePlan.model_validate(updated_plan)
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.delete("/plans/{plan_id}")
+async def delete_plan(
+    plan_id: int,
+    db_session: DB_SESSION
+) -> dict[str, str]:
+    try:
+        result_of_delete = await PlanService.delete_plan(
+            db_session=db_session,
+            plan_id=plan_id
+        )
+        if result_of_delete:
+            return {"message":
+                    "plan was deleted"}
+        else:
+            raise ServiceException(
+                "Plan wasn't deleted"
+            )
+    except ServiceException as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+    except NotFoundException as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
 
 
 @router.get("/categories")
-async def get_categories(
-        db_session: DB_SESSION):
-    categories = await ExpenseDAO.find_categories(db_session)
-    return categories
+async def get_all_categories(
+    db_session: DB_SESSION
+) -> list[ResponseCategory]:
+    try:
+        categories = await CategoryService.get_all_categories(
+            db_session=db_session
+        )
+        return [ResponseCategory.model_validate(category)
+                for category in categories]
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+    except NotFoundException as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
+
+@router.post("/categories")
+async def create_category(
+    category: BaseCategory,
+    db_session: DB_SESSION
+) -> ResponseCategory:
+    try:
+        new_category = await CategoryService.create_category(
+            db_session=db_session,
+            category=category
+        )
+        return ResponseCategory.model_validate(new_category)
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@router.delete("/categories/{category_id}")
+async def delete_category(
+    category_id: int,
+    db_session: DB_SESSION
+) -> dict[str, str]:
+    try:
+        result_of_delete = await CategoryService.delete_category(
+            db_session=db_session,
+            category_id=category_id
+        )
+        if result_of_delete:
+            return {"message":
+                    "category was deleted"}
+        else:
+            raise ServiceException(
+                "Category wasn't deleted"
+            )
+    except ServiceException as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+    except DatabaseException as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
