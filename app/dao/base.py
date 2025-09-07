1a6a6c24395f83adc9d8ba67150fb3da0b712e65
Hakujona <stlesha1992@gmail.com>
2025-09-07 16:41:41 +0300

    Remove transaction control from BaseDAO

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,43 +1,75 @@
-from sqlalchemy import select, delete
+from typing import (
+    Type, TypeVar, Generic,
+    List, Optional
+)
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import SQLAlchemyError
+
+T = TypeVar("T")
 
 
-class BaseDAO:
-    model = None
-
-    @classmethod
-    async def find_all(cls, session: AsyncSession, **filter_by):
-        query = select(cls.model).filter_by(**filter_by)
-        result = await session.execute(query)
-        return result.scalars().all()
+class BaseDAO(Generic[T]):
+    model: Type[T] = None
 
     @classmethod
-    async def find_one_or_none(cls, session:AsyncSession, **filter_by):
+    async def find_all(
+        cls,
+        db_session: AsyncSession,
+        **filter_by
+    ) -> List[T]:
         query = select(cls.model).filter_by(**filter_by)
-        result = await session.execute(query)
+        result = await db_session.execute(query)
+        return list(result.scalars().all())
+
+    @classmethod
+    async def find_one_or_none(
+        cls,
+        db_session: AsyncSession,
+        **filter_by
+    ) -> Optional[T]:
+        query = select(cls.model).filter_by(**filter_by)
+        result = await db_session.execute(query)
         return result.scalars().one_or_none()
 
     @classmethod
-    async def add(cls, session: AsyncSession, **values):
+    async def add(
+        cls,
+        db_session: AsyncSession,
+        **values
+    ) -> T:
         new_instance = cls.model(**values)
-        session.add(new_instance)
-        try:
-           await session.commit()
-        except SQLAlchemyError as e:
-            await session.rollback()
-            raise e
-        await session.refresh(new_instance)
+        db_session.add(new_instance)
         return new_instance
 
     @classmethod
-    async def delete(cls, session: AsyncSession, data_id: int):
-        query = delete(cls.model).filter_by(id=data_id)
-        await session.execute(query)
-        try:
-            await session.commit()
-        except SQLAlchemyError as e:
-            await session.rollback()
-            raise e
-        return {"message":
-                "data was deleted"}
+    async def update_instance(
+        cls,
+        db_session: AsyncSession,
+        data_id: int,
+        **values
+    ) -> Optional[T]:
+        instance = await cls.find_one_or_none(
+            db_session=db_session,
+            id=data_id
+        )
+        if not instance:
+            return None
+        for field, value in values.items():
+            setattr(instance, field, value)
+        # db_session.add(instance=instance)
+        return instance
+
+    @classmethod
+    async def delete_instance(
+        cls,
+        db_session: AsyncSession,
+        data_id: int
+    ) -> bool:
+        instance = cls.find_one_or_none(
+            db_session=db_session,
+            id=data_id
+        )
+        if not instance:
+            return False
+        await db_session.delete(instance=instance)
+        return True
